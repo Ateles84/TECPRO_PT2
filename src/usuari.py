cd0d3c0ff18a08fcf7daeebf8d3da89092313534
Joan <juanbass84@gmail.com>
2021-03-21 22:57:25 +0100

    prova-tonta

--- src/usuari.py
+++ src/usuari.py
@@ -40,6 +40,9 @@ class Usuari(object):
 
         return "Usuari: "+self.nick+" Email: "+self.__email+" Encripted password: "+encPass
 
+    def lel(self):
+        print("vaya tolo")
+
 if __name__=='__main__':
     u = Usuari('Bernat', 'b@gmail.com', 'bbrunet')
     print(u)
